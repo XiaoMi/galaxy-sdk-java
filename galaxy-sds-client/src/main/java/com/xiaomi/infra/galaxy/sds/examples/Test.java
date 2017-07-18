ec1f518a24365141dd0335628cb2854c464f4192
linshangquan <linshangquan@xiaomi.com>
2017-07-18 17:10:02 +0800

    remove useless files in sds sdk

--- galaxy-sds-client/src/main/java/com/xiaomi/infra/galaxy/sds/examples/Test.java
+++
@@ -1,9 +0,0 @@
-package com.xiaomi.infra.galaxy.sds.examples;
-
-/**
- * Copyright 2015, Xiaomi.
- * All rights reserved.
- * Author: linshangquan@xiaomi.com
- */
-public class Test {
-}
--- galaxy-sds-client/src/main/java/com/xiaomi/infra/galaxy/sds/examples/stream/TalosConsumerDemo.java
+++
@@ -1,9 +0,0 @@
-package com.xiaomi.infra.galaxy.sds.examples.stream;
-
-/**
- * Copyright 2015, Xiaomi.
- * All rights reserved.
- * Author: linshangquan@xiaomi.com
- */
-public class TalosConsumerDemo {
-}
